81564a90b417a0de89bdccc7a822fab9096d3584
Arjun C Vinod <68469520+arjuncvinod@users.noreply.github.com>
2025-11-13 18:15:15 +0530

    feat: add loading indicator to search results component

--- src/app/features/home/search-results/search-results.component.ts
+++ src/app/features/home/search-results/search-results.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HotelService } from '../../../services/hotel.service';
 import { SearchFilterComponent } from './components/search-filter/search-filter.component';
 import { SearchInfoComponent } from './components/search-info/search-info.component';
 import { HotelCardComponent } from './components/hotel-card/hotel-card.component';
+import { LoaderComponent } from '../../../shared/loader/loader.component';
 
 @Component({
   selector: 'app-search-results',
-  imports: [SearchFilterComponent,SearchInfoComponent,HotelCardComponent],
+  imports: [SearchFilterComponent,SearchInfoComponent,HotelCardComponent,LoaderComponent],
   templateUrl: './search-results.component.html',
   styleUrl: './search-results.component.scss'
 })
@@ -18,6 +19,8 @@ export class SearchResultsComponent {
   checkOut: string | null = '';
   sortBy: string | null = '';
   price: string | null = '';
+
+  isLoading = signal(true);
 
   hotels: any[] = [];
 
@@ -36,9 +39,11 @@ export class SearchResultsComponent {
         this.hotelService.getAvailableRooms(this.location, this.checkIn, this.checkOut, this.sortBy ,this.price).subscribe({
           next: (data) => {
             this.hotels = data;
+            this.isLoading.set(false);
           },
           error: (err) => {
             console.error('Error fetching available rooms', err);
+            this.isLoading.set(false);
           }
         });
       }
